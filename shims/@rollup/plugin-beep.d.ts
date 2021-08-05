ac1ae8643be1958d95b40ce728e59df7d7b141dc
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-08-05 12:19:28 +0530

    docs: add TODO for rollup/plugin-beep

--- shims/@rollup/plugin-beep.d.ts
+++ shims/@rollup/plugin-beep.d.ts
@@ -1,3 +1,4 @@
+// @TODO: Remove this once https://github.com/rollup/plugins/pull/969
 declare module '@rollup/plugin-beep' {
   import { Plugin } from 'rollup';
   export default function beep(): Plugin;
