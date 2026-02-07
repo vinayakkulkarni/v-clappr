835ee76be33f6d7d57b12d6edf52225e6480f619
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-02-08 04:11:47 +0530

    refactor: update vite.config.ts for ESM-only output

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,16 @@
+import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import pkg from './package.json';
-
-const banner = `/*!
- * ${pkg.name} v${pkg.version}
- * ${pkg.description}
- * (c) ${new Date().getFullYear()} ${pkg.author.name}<${pkg.author.email}>
- * Released under the ${pkg.license} License
- */
-`;
 
 export default defineConfig({
+  plugins: [vue()],
   build: {
     target: 'esnext',
     sourcemap: true,
-    reportCompressedSize: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VClappr',
-      formats: ['es', 'cjs', 'umd'],
-      fileName: pkg.name,
-    },
-    commonjsOptions: {
-      extensions: ['.js', '.ts', '.vue'],
-      strictRequires: true,
-      transformMixedEsModules: true,
-      exclude: 'src/**',
-      include: 'node_modules/**',
+      formats: ['es'],
+      fileName: 'v-clappr',
     },
     rollupOptions: {
       external: [
@@ -37,18 +20,8 @@ export default defineConfig({
         '@clappr/hlsjs-playback',
       ],
       output: {
-        banner,
         exports: 'named',
-        strict: true,
-        extend: true,
-        globals: {
-          vue: 'vue',
-          '@clappr/core': 'core',
-          '@clappr/plugins': 'plugins',
-          '@clappr/hlsjs-playback': 'HlsjsPlayback',
-        },
       },
     },
   },
-  plugins: [vue()],
 });
